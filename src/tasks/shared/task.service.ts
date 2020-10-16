722c0bd299d34b4cf7942a5d47cc76da2db43ff5
caioagiani <agianicaio@gmail.com>
2020-10-15 21:51:07 -0300

    feat: added static crud

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService { 
+  tasks: Task[] = [
+    { id: 1, description: 'Task 1', completed: false },
+    { id: 2, description: 'Task 2', completed: false },
+    { id: 3, description: 'Task 3', completed: true },
+    { id: 4, description: 'Task 4', completed: false },
+    { id: 5, description: 'Task 5', completed: false },
+    { id: 6, description: 'Task 6', completed: false },
+    { id: 7, description: 'Task 7', completed: false },
+    { id: 8, description: 'Task 8', completed: false },
+    { id: 9, description: 'Task 9', completed: false },
+    { id: 10, description: 'Task 10', completed: true }
+  ];
+
+  getAll() {
+    return this.tasks;
+  }
+
+  getById(id: number) {
+    const task = this.tasks.find((value) => value.id == id); 
+
+    return task;
+  }
+
+  create(task: Task) {
+    let lastId = 0;
+
+    if (this.tasks.length > 0) {
+      lastId = this.tasks[this.tasks.length - 1].id;
+    }
+
+    task.id = lastId + 1;
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  update(task: Task) {
+    const taskArray = this.getById(task.id);
+
+    if (taskArray) {
+      taskArray.description = task.description;
+      taskArray.completed =  task.completed;
+    }
+
+    return taskArray;
+  }
+
+  delete(id: number) {
+    const index = this.tasks.findIndex((value) => value.id == id);
+
+    this.tasks.splice(index, 1);
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Body, 
+  Controller, 
+  Delete, 
+  Get, 
+  Param, 
+  Post, 
+  Put 
+} from '@nestjs/common';
+import { Task } from './shared/task';
+import { TaskService } from './shared/task.service';
+
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TaskService) {}
+
+  @Get()
+  async getAll(): Promise<Task[]> {
+    return this.tasksService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<Task> {
+    return this.tasksService.getById(id);
+  }
+
+  @Post()
+  async create(@Body() task: Task): Promise<Task> {
+    return this.tasksService.create(task);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+    task.id = id;
+
+    return this.tasksService.update(task);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    this.tasksService.delete(id)
+  }
+}
